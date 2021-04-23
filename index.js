47a6c5c8a302b141f1f096afa7fb8bca9f3eacaa
Avi <Avg8377@gmail.com>
2021-04-23 13:41:07 +0530

    first attempt

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 // app.use(express.static("public"))npm start;
-console.log("hello world");
+console.log("hello world in EC@");
 app.listen(3000, console.log("listening on port 3000"));
