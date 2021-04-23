9949c9aae80fc8248d7d1265ebd2293e03e133cf
Avi <Avg8377@gmail.com>
2021-04-23 13:19:38 +0530

    kdfksa

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+// app.use(express.static("public"))npm start;
+console.log("hello world");
+app.listen(3000, console.log("listening on port 3000"));
